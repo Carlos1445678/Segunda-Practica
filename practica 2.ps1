61d1a5e64f321c7272636ebbd9c46720bf2f17ab
Carlos1445678 <cpatflrs@gmail.com>
2021-09-02 19:53:57 -0500

    Presento la segunda practica

---
+++ practica 2.ps1
@@ -0,0 +1,30 @@
+﻿$Path = "C:\practica 2"
+
+for ($i-1;$i -lt 6;$i++) {
+   
+    if ($i - 1) { 
+        
+        New-item -path "$Path\Carpeta 1" -ItemType Directory
+
+       
+    } if ($i - 2) { 
+        
+        New-item -path "$Path\Carpeta 2" -ItemType Directory 
+
+       
+    } if ($i - 3) { 
+        
+        New-item -path "$Path\Carpeta 3" -ItemType Directory 
+
+      
+    } if ($i - 4) { 
+        
+        New-item -path "$Path\Carpeta 4" -ItemType Directory 
+
+       
+    } if ($i - 5) { 
+        
+        New-item -path "$Path\Carpeta 5" -ItemType Directory 
+
+    } 
+}
